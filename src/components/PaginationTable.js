c228cf60344e16b5ddf2a34d2d8af811a9136b8c
Jesús Guerrero <jesus.guerrero@talan.com>
2022-06-06 12:56:21 +0200

    Adding more Pagination Elements

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -19,11 +19,16 @@ const PaginationTable = () => {
     canNextPage,
     canPreviousPage,
     pageOptions,
+    gotoPage,
+    pageCount,
     state,
   } = useTable(
     {
       columns,
       data,
+      initialState: {
+        pageIndex: 0,
+      },
     },
     usePagination
   );
@@ -62,11 +67,31 @@ const PaginationTable = () => {
             {pageIndex + 1} of {pageOptions.length}
           </strong>{" "}
         </span>
+        <span>
+          | Go to page:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: "50px" }}
+          />
+        </span>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>
         <button onClick={() => previousPage()} disabled={!canPreviousPage}>
           Previous
         </button>
         <button onClick={() => nextPage()} disabled={!canNextPage}>
           Next
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
         </button>
       </div>
     </>
