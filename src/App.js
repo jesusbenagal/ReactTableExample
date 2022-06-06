6b7bf503e8e2103fd8f7b0d2d81f3ca84b5c564c
Jesús Guerrero <jesus.guerrero@talan.com>
2022-06-06 13:31:21 +0200

    SelectRow added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import BasicTable from "./components/BasicTable";
 import FilteringTable from "./components/FilteringTable";
 import PaginationTable from "./components/PaginationTable";
+import RowSelection from "./components/RowSelection";
 import SortingTable from "./components/SortingTable";
 
 function App() {
   return (
-    <div className="App">
-      <PaginationTable />
+    <div>
+      <RowSelection />
     </div>
   );
 }
