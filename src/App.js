49aad187ce1dd984c3aaf0208da1987008be2273
Jesús Guerrero <jesus.guerrero@talan.com>
2022-06-06 12:43:45 +0200

    Adding basic Pagination

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import BasicTable from "./components/BasicTable";
 import FilteringTable from "./components/FilteringTable";
+import PaginationTable from "./components/PaginationTable";
 import SortingTable from "./components/SortingTable";
 
 function App() {
   return (
     <div className="App">
-      <FilteringTable />
+      <PaginationTable />
     </div>
   );
 }
---
+++ src/components/PaginationTable.js
@@ -0,0 +1,76 @@
+import React, { useMemo } from "react";
+import { useTable, usePagination } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./columns";
+import "./table.css";
+
+const PaginationTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    previousPage,
+    prepareRow,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    state,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    usePagination
+  );
+
+  const { pageIndex } = state;
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default PaginationTable;
